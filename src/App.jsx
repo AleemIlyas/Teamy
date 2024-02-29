1779e9d1123b62c08db7d63a815f064fdcacc9b4
AleemIlyas <aleemempire215@gmail.com>
2024-02-29 23:02:24 +0500

    adding members page and changed header.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,60 @@
-import React , { Suspense } from 'react'
-import { Route , Routes , Outlet } from 'react-router-dom'
-import {  useSelector } from 'react-redux'
-import './App.css'
-import HomePage from './HomePage/HomePage'
-const Layout = React.lazy(()=>import('./Project/Layout/Layout'))
-const Tasks = React.lazy(()=>import('./Project/Components/Tasks/Tasks'))
-const ShowDetail = React.lazy(()=>('./Project/Container/showDetail/showDetail'))
-import Calander from './Project/Calander/Calander'
-import Files from './Project/Files/Files'
-import Activity from './Project/Activity/Activity.jsx'
-import DashBoard from './Project/Dashboard/Dashboard'
-import Login from './Project/LogIn/login.jsx'
-import SignUP from './Project/SignUp/SignUp'
-import Spinner from './Project/Components/UI/Spinner/Spinner'
-import Team from './Project/Container/Team/Team'
-import MyTask from './Project/MyTasks/MyTask'
-
+import React, { Suspense } from "react";
+import { Route, Routes, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./App.css";
+import HomePage from "./HomePage/HomePage";
+const Layout = React.lazy(() => import("./Project/Layout/Layout"));
+const Tasks = React.lazy(() => import("./Project/Components/Tasks/Tasks"));
+const ShowDetail = React.lazy(
+  () => "./Project/Container/showDetail/showDetail"
+);
+import Calander from "./Project/Calander/Calander";
+import Files from "./Project/Files/Files";
+import Activity from "./Project/Activity/Activity.jsx";
+import DashBoard from "./Project/Dashboard/Dashboard";
+import Login from "./Project/LogIn/login.jsx";
+import SignUP from "./Project/SignUp/SignUp";
+import Spinner from "./Project/Components/UI/Spinner/Spinner";
+import Team from "./Project/Container/Team/Team";
+import MyTask from "./Project/MyTasks/MyTask";
 
 function App() {
-    const isLoggedin = useSelector((state)=>state.user.isLoggedIn)
+  const isLoggedin = useSelector((state) => state.user.isLoggedIn);
   return (
     <React.Fragment>
       <Suspense fallback={<Spinner />}>
-    <Routes>
-      <Route path='/' element={ <HomePage /> } />
-      { !isLoggedin ?  
-      <>
-      <Route path='/login' element= { <Login /> } />
-      <Route path='/signup' element= { <SignUP /> } />
-      <Route path='*' element={ <h1 className='flex text-center'>Page Not Found!</h1> } />
-      </>
-          :
-        <Route path='/dashboard/*' element={ <Layout /> }>
-        <Route index path=''  element={ <DashBoard /> } />
-        <Route  path='myTasks'  element={ <MyTask /> } />
-        <Route path='project/:id/*' element={ <Outlet /> }>
-          <Route path='activity' element={<Activity /> } />
-          <Route path='calander' element={<Calander /> } />
-          <Route path='files' element={<Files /> } />
-          <Route path='' element={ <Tasks /> }  >
-            <Route path="todo/:taskId" element={ <ShowDetail /> } />
-            <Route path="backlog/:taskId" element={ <ShowDetail /> } />
-          </Route>
-        </Route>
-        <Route path='Team/:id' element={ <Team /> } />
-        </Route>
-        
-      }
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {!isLoggedin ? (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUP />} />
+              <Route
+                path="*"
+                element={<h1 className="flex text-center">Page Not Found!</h1>}
+              />
+            </>
+          ) : (
+            <Route path="/dashboard/*" element={<Layout />}>
+              <Route index path="" element={<DashBoard />} />
+              <Route path="myTasks" element={<MyTask />} />
+              <Route path="Members" element={<MyTask />} />
+              <Route path="project/:id/*" element={<Outlet />}>
+                <Route path="activity" element={<Activity />} />
+                <Route path="calander" element={<Calander />} />
+                <Route path="files" element={<Files />} />
+                <Route path="" element={<Tasks />}>
+                  <Route path="todo/:taskId" element={<ShowDetail />} />
+                  <Route path="backlog/:taskId" element={<ShowDetail />} />
+                </Route>
+              </Route>
+              <Route path="Team/:id" element={<Team />} />
+            </Route>
+          )}
         </Routes>
-        </Suspense>
+      </Suspense>
     </React.Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Project/SideNav/components/Header/header.jsx
+++ src/Project/SideNav/components/Header/header.jsx
@@ -1,80 +1,128 @@
-import React , { useEffect } from 'react'
-import styles from './header.module.css'
-import Image1 from '/Projects/1.svg'
-import { FiMoreHorizontal } from 'react-icons/fi'
-import { NavLink  ,useParams } from 'react-router-dom'
-import { useLocation  } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
+import React, { useEffect } from "react";
+import styles from "./header.module.css";
+import Image1 from "/Projects/1.svg";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { NavLink, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header(props) {
-    const {id} = useParams()
-    const loc = useLocation()
-    const project = useSelector(state=>state.project.projects)
-    const teams = useSelector(state=>state.teams.Teams)
-    const links =  [ '/dashboard' , '/dashboard/myTasks' , '/dashboard/Notification' ]
+  const { id } = useParams();
+  const loc = useLocation();
+  const project = useSelector((state) => state.project.projects);
+  const teams = useSelector((state) => state.teams.Teams);
+  const links = [
+    "/dashboard",
+    "/dashboard/myTasks",
+    "/dashboard/Notification",
+    "/dashboard/Members",
+  ];
 
-    const headerTextMap = {
-        '/dashboard': 'Dashboard',
-        '/dashboard/myTasks': 'MY TASKS',
-        '/dashboard/Notification': 'Notifications',
-        // Add more page links and header texts as needed
-      };
+  const headerTextMap = {
+    "/dashboard": "Dashboard",
+    "/dashboard/myTasks": "MY TASKS",
+    "/dashboard/Notification": "Notifications",
+    "/dashboard/Members": "Members",
+    // Add more page links and header texts as needed
+  };
 
-      project.forEach((project) => {
-        const projectPath = `/dashboard/project/${project._id}`;
-        headerTextMap[projectPath] = project.title;
-        [ '/Activity' , '/files' , '/Calander' ].forEach((val)=>{
-            headerTextMap[projectPath+val] = project.title
-        })
-      
-      });
+  project.forEach((project) => {
+    const projectPath = `/dashboard/project/${project._id}`;
+    headerTextMap[projectPath] = project.title;
+    ["/Activity", "/files", "/Calander"].forEach((val) => {
+      headerTextMap[projectPath + val] = project.title;
+    });
+  });
 
+  teams.forEach((team) => {
+    const projectPath = `/dashboard/Team/${team._id}`;
+    headerTextMap[projectPath] = team.title;
+    links.push(projectPath);
+  });
 
-      teams.forEach((team) => {
-        const projectPath = `/dashboard/Team/${team._id}`;
-        headerTextMap[projectPath] = team.title;
-        links.push(projectPath)
-        })
-    
-
-
-    console.log(links.includes(loc.pathname))
-    useEffect(()=>{
-        console.log('Component Did Mount')
-    } , [props])
-    // classes list for active and inactive links
-    let Active = ['inline-flex',styles.active].join(' ') 
-    let inActive = ['inline-flex']
+  useEffect(() => {
+    console.log("Component Did Mount");
+  }, [props]);
+  // classes list for active and inactive links
+  let Active = ["inline-flex", styles.active].join(" ");
+  let inActive = ["inline-flex"];
 
   return (
     <React.Fragment>
-    <div className={['bg-white flex flex-col',styles.Header].join(' ')}>
-           <div className='w-11/12 m-auto flex items-center justify-between'>
-                <div className='flex'>
-                <img className='mr-3' width={30} src={Image1} alt="" />
-                <h1 className='text-xl font-bold tracking-wide'>{headerTextMap[loc.pathname]  || 'HEADER'}</h1>
-                </div>
-                <div>
-                <FiMoreHorizontal className='text-3xl' />
-                </div>
-            <div className='flex space-x-1'>
-                <img width={40} className='rounded-full' src="https://avatars2.githubusercontent.com/u/6184" alt="" />
-                <img width={40} className='rounded-full' src="https://avatars2.githubusercontent.com/u/6181" alt="" />
-                <img width={40} className='rounded-full' src="https://avatars2.githubusercontent.com/u/6176" alt="" />
-            </div>
+      <div className={["bg-white flex flex-col", styles.Header].join(" ")}>
+        <div className="w-11/12 m-auto flex items-center justify-between">
+          <div className="flex">
+            <img className="mr-3" width={30} src={Image1} alt="" />
+            <h1 className="text-xl font-bold tracking-wide">
+              {headerTextMap[loc.pathname] || "HEADER"}
+            </h1>
+          </div>
+          <div>
+            <FiMoreHorizontal className="text-3xl" />
+          </div>
+          <NavLink to={"/dashboard/Members"} className="flex space-x-1">
+            <img
+              width={40}
+              className="rounded-full"
+              src="https://avatars2.githubusercontent.com/u/6184"
+              alt=""
+            />
+            <img
+              width={40}
+              className="rounded-full"
+              src="https://avatars2.githubusercontent.com/u/6181"
+              alt=""
+            />
+            <img
+              width={40}
+              className="rounded-full"
+              src="https://avatars2.githubusercontent.com/u/6176"
+              alt=""
+            />
+          </NavLink>
         </div>
-        {
-            !links.includes(loc.pathname) ? 
-            <ul className='w-11/12 mr-auto ml-auto space-x-4'>
-                <NavLink className={({isActive,isPending})=>isPending?inActive:isActive?Active:inActive} to={`project/${id}`} end> <span className='mb-2.5'>Tasks</span></NavLink>
-                <NavLink className={({isActive,isPending})=>isPending?inActive:isActive?Active:inActive} to={`project/${id}/Activity`}> <span className='mb-2.5'>Activity</span></NavLink>
-                <NavLink className={({isActive,isPending})=>isPending?inActive:isActive?Active:inActive} to={`project/${id}/Calander`} > <span className='mb-2.5'>Calander</span></NavLink>
-                <NavLink className={({isActive,isPending})=>isPending?inActive:isActive?Active:inActive} to={`project/${id}/files`}> <span className='mb-2.5'>Files</span></NavLink>
-            </ul>
-            : null
-        }
-     </div>
-     </React.Fragment>
-  )
+        {!links.includes(loc.pathname) ? (
+          <ul className="w-11/12 mr-auto ml-auto space-x-4">
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? inActive : isActive ? Active : inActive
+              }
+              to={`project/${id}`}
+              end
+            >
+              {" "}
+              <span className="mb-2.5">Tasks</span>
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? inActive : isActive ? Active : inActive
+              }
+              to={`project/${id}/Activity`}
+            >
+              {" "}
+              <span className="mb-2.5">Activity</span>
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? inActive : isActive ? Active : inActive
+              }
+              to={`project/${id}/Calander`}
+            >
+              {" "}
+              <span className="mb-2.5">Calander</span>
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? inActive : isActive ? Active : inActive
+              }
+              to={`project/${id}/files`}
+            >
+              {" "}
+              <span className="mb-2.5">Files</span>
+            </NavLink>
+          </ul>
+        ) : null}
+      </div>
+    </React.Fragment>
+  );
 }
